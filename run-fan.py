cbcbc03aa06f844ca327546072ba0436ab837c22
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2019-06-30 23:49:26 +0200

    Bugfix
    Make use of the sleep functionality.

--- run-fan.py
+++ run-fan.py
@@ -46,6 +46,6 @@ try:
     setup() 
     while True:
         getTEMP()
-    sleep(5) # Read the temperature every 5 sec, increase or decrease this limit if you want
+        sleep(5) # Read the temperature every 5 sec, increase or decrease this limit if you want
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt 
     GPIO.cleanup() # resets all GPIO ports used by this program
